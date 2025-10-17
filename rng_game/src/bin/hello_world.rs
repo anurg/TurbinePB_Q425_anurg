d910d5cff91bdbec47c46820adb018353baaa153
anunymous <anurg@yahoo.com>
2025-10-17 18:14:34 +0000

    added rustlings

--- rng_game/src/bin/hello_world.rs
+++ rng_game/src/bin/hello_world.rs
@@ -2,8 +2,10 @@ use chrono::{Datelike, Local};
 
 fn main() {
     let now = std::time::Instant::now();
+
     let result = Local::now().weekday();
     println!(" Hello World! - Today is - {:?}", result);
+
     let result = now.elapsed();
     println!("Program executed in seconds: {:?}", result);
 }
