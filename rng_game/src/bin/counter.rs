af7ee01e2fc66d224c7a3522aaee274ae863e454
anurg <anurg@yahoo.com>
2025-10-17 09:08:53 +0000

    counter program added.

--- rng_game/src/bin/counter.rs
+++ rng_game/src/bin/counter.rs
@@ -1,3 +1,20 @@
+use std::io;
+use std::{thread::sleep, time::Duration};
 fn main() {
     println!("Hello, world! from Main");
+    let mut counter = Box::new(0);
+    'outer: loop {
+        for _i in [0..1000000] {
+            *counter += 1;
+            sleep(Duration::new(0, 100000000));
+            println!("{}", counter);
+            if *counter % 10 == 0 {
+                println!("Do you want counter to stop(y/n)?");
+                let result = io::stdin().lines().next().unwrap().unwrap();
+                if result == "y" {
+                    break 'outer;
+                }
+            }
+        }
+    }
 }
