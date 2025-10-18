e33d062268fa7aec55b601502cb86f023398a354
anurg <anurg@yahoo.com>
2025-10-18 09:03:34 +0000

    rustlings -strings

--- rustlings/exercises/09_strings/strings3.rs
+++ rustlings/exercises/09_strings/strings3.rs
@@ -1,13 +1,25 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+
+    // Solution-1
+    // input.to_string().push_str("world!");
+    // input.to_string()
+
+    // // Solution-2
+    // input.to_string() + "World"
+
+    // Solution-3
+    format!("{input} world!")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
